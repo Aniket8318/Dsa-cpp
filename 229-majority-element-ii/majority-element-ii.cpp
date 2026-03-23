21e4f8b60a43c4cf581875019debbe24ccc842ab
Aniket8318 <147974095+Aniket8318@users.noreply.github.com>
2026-03-23 23:48:52 +0530

    Time: 7 ms (41.95%) | Memory: 26.3 MB (27.50%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -0,0 +1,22 @@
+
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        unordered_map<int,int>freq;
+        vector<int>ans;
+        int n=nums.size()/3;
+        for(auto num: nums)
+        {
+            freq[num]++;
+        }
+        for(auto num: freq)
+        {
+            if(num.second>n)
+            {
+                ans.push_back(num.first);
+            }
+        }
+        return ans;
+    }
+};
