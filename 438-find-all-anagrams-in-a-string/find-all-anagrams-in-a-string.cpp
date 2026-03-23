7211063ff863dda534c245a47d7cfc9dd52ee4a0
Aniket8318 <147974095+Aniket8318@users.noreply.github.com>
2026-03-24 01:55:50 +0530

    Time: 3 ms (77.44%) | Memory: 11.7 MB (36.17%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        
+        vector<int> ans;
+        unordered_map<char,int> mp;
+
+        for(char ch : p) {
+            mp[ch]++;
+        }
+
+        int i = 0, j = 0;
+        int count = mp.size();
+
+        while(j < s.size()) {
+
+            if(mp.find(s[j]) != mp.end()) {
+                mp[s[j]]--;
+
+                if(mp[s[j]] == 0)
+                    count--;
+            }
+
+            if(j - i + 1 < p.size()) {
+                j++;
+            }
+
+            else if(j - i + 1 == p.size()) {
+
+                if(count == 0)
+                    ans.push_back(i);
+
+                if(mp.find(s[i]) != mp.end()) {
+                    mp[s[i]]++;
+
+                    if(mp[s[i]] == 1)
+                        count++;
+                }
+
+                i++;
+                j++;
+            }
+        }
+
+        return ans;
+    }
+};
