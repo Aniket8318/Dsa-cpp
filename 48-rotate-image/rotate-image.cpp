8f770721c45f6df12a34b4a3e771dd0ead031713
Aniket8318 <147974095+Aniket8318@users.noreply.github.com>
2026-04-10 00:43:55 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (8.86%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -7,7 +7,7 @@ public:
 
         for(int i=0;i<n-1;i++)
         {
-            for(int j=i+1;j<n;j++)
+            for(int j=i;j<n;j++)
             {
                 swap(matrix[i][j],matrix[j][i]);
             }
